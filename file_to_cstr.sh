74dbeeda3ce065f0b7aa498c1e2702497fad0dd2
W. Michael Brown <brownw@ornl.gov>
2010-11-08 18:42:14 +0000

    Geryon: New file_to_cstr by Axel Kohlmeyer

--- file_to_cstr.sh
+++ file_to_cstr.sh
@@ -1,16 +1,47 @@
-#!/bin/csh
-@ num_args = $#argv
-set output = $argv[$num_args]
-/bin/rm -f $output
-touch $output
+#!/bin/sh
 
-@ i = 1
-while ( $i < $num_args )
-  set source = $argv[$i]
-  set kernel_name = $argv[$i]:r:t
-  echo "Converting $source to a cstring in $output"
-  echo "const char * $kernel_name = \" >> $output
-  sed 's/\\/\\\\/g' $source | sed 's/"/\\"/g' | awk '$1!="//" && $1!=".file"{printf(" \"%s\\n\"\n",$0)}END{printf(" ;\n")}' >> $output
-  @ i = $i + 1
-end
+# convert ptx assembly output into 
+# a c-style string constant written
+# in portable posix shell script.
+# requires: sed, rm, mv
+#
+# Author: Axel Kohlmeyer, Temple University
+
+num_args=$#
+
+# we write to a scratch file, since
+# we know the real file name only at
+# the very end.
+output=geryon.tmp.$$
+: > $output
+
+# remove temporary file in case we're interrupted. 
+cleanup () {
+  rm -f geryon.tmp.$$
+}
+trap cleanup INT QUIT TERM
+
+# loop over arguments and convert to 
+# string constants. 
+i=1
+while [ $i -lt $num_args ]
+do \
+  src=$1
+  krn=${src##*/}
+  krn=${krn%.*}
+  echo "Converting kernel $krn from $src to a c-style string"
+  echo "const char * $krn = " >> $output
+  sed -e 's/\\/\\\\/g'   \
+      -e 's/"/\\"/g'     \
+      -e 's/ *\/\/.*$//' \
+      -e '/\.file/D'     \
+      -e '/^[ 	]*$/D'   \
+      -e 's/^\(.*\)$/"\1\\n"/' $src >> $output
+  echo ';' >> $output
+  shift
+  i=`expr $i + 1`
+done
+
+# $1 holds now the real output file name
+mv $output $1
 
